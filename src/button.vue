c7dcd91a2c2200ade89ab0fff7b5a11271512d13
x1059455449 <1059455449@qq.com>
2019-03-16 03:14:51 +0800

    优化代码

---
+++ src/button.vue
@@ -0,0 +1,76 @@
+<template>
+    <button class="g-button" :class="{[`icon-${iconPosition}`]:true}">
+        <svg v-if="icon" class="icon"><use :xlink:href="`#i-${icon}`" ></use></svg>
+        
+        <div class="content">
+            <slot></slot>
+        </div>
+    </button>
+</template>
+
+<script>
+    export default {
+       //props:['icon','iconPosition']
+       props:{
+           icon:{},
+           iconPosition:{
+               type:String,
+               default:'left',
+                //属性检查器
+                validator (value) {
+                    console.log(value);
+                    // if(value !== 'left' && value !== 'right') {
+                    //     console.log('please input left or right')
+                    //     return false
+                    // } else {
+                    //     return true
+                    // }
+                    //优化
+                    return !(value !== 'left' && value !== 'right')
+                    //return value !== 'left' && value !== 'right' ? false : true;
+                }
+           }
+       }
+    }
+</script>
+
+<style  lang="scss">
+    .g-button {
+            font-size: var(--font-size);
+            height: var(--button-height);
+            padding: 0 1em;
+            border-radius: var(--border-radius);
+            border: 1px solid var(--border-color);
+            background: var(--button-bg);
+            display: inline-flex;
+            justify-content: center;
+            align-items: center;
+            vertical-align: middle;
+        &:active {
+            background-color: var(--button-active-bg);
+        }
+        &:hover {
+            border-color: var(--border-color-hover);
+        }
+        &:focus {
+            outline: none;
+        }
+        > .icon {
+            order: 1;
+            margin-right: .3em;
+        }
+        > .content {
+            order: 2;
+        }
+        &.icon-right {
+            > .icon {
+                order: 2;
+                margin-right: 0;
+                margin-left: .3em;
+            }
+            > .content {
+                order: 1;
+            }
+        }
+    }       
+</style>
