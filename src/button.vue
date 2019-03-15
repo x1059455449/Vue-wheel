54f342a3fca397e0b245d83df2cc87c44510f84a
x1059455449 <1059455449@qq.com>
2019-03-16 03:45:12 +0800

    svg in icon.vue

--- src/button.vue
+++ src/button.vue
@@ -1,7 +1,7 @@
 <template>
     <button class="g-button" :class="{[`icon-${iconPosition}`]:true}">
-        <svg v-if="icon" class="icon"><use :xlink:href="`#i-${icon}`" ></use></svg>
-        
+        <!-- <svg v-if="icon" class="icon"><use :xlink:href="`#i-${icon}`" ></use></svg> -->
+        <g-icon v-if="icon" :name="icon" class="icon"></g-icon>
         <div class="content">
             <slot></slot>
         </div>
