0f6f90c58cf0ac8e6cd94f71e0e272498f959ae6
x1059455449 <1059455449@qq.com>
2019-04-20 18:22:43 +0800

    bushu

--- deploy.sh
+++ deploy.sh
@@ -17,9 +17,9 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-git push -f git@github.com:x1059455449/Vue-wheel.github.io.git master
+#git push -f git@github.com:x1059455449/Vue-wheel.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
- #git push -f git@github.com:x1059455449/Vue-wheel.git master:gh-pages
+ git push -f git@github.com:x1059455449/Vue-wheel.git master:gh-pages
 
 cd -
